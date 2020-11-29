2efa008fabe78c0159326ad83a42daf0e28a76af
Pimm "de Chinchilla" Hogeling <pimmhogeling@gmail.com>
2020-11-29 20:15:00 +0100

    Type(Script) fixes for runIf.

--- index.d.ts
+++ index.d.ts
@@ -1,35 +1,56 @@
 type Nullish = null | undefined;
 type Resolve<T> = T extends Promise<infer A> ? A : T;
+type Promisify<T> = T extends Promise<any> ? T : Promise<T>;
+type IfPromise<X, T, F> = X extends Promise<any> ? T : F;
 type ExtractResolved<T, U> = T extends Promise<infer A> ? A extends U ? Promise<A> : never : never;
-type Swap1<T, B> = T extends Promise<any> ? Promise<Resolve<B>> : B;
-type Swap2<T, U, B> = T extends Promise<any> ? Promise<Resolve<B>> : U extends Promise<any> ? Promise<Resolve<B>> : B;
-type Swap3<T, U, V, B> = T extends Promise<any> ? Promise<Resolve<B>> : U extends Promise<any> ? Promise<Resolve<B>> : V extends Promise<any> ? Promise<Resolve<B>> : B;
+type Ever<T> = T extends Promise<never> ? never : T;
 /**
  * Calls the passed callback, forwarding the value and routing back whatever is returned.
  */
-declare function run<T, R, C>(this: C, value: T, callback: (this: C, value: Resolve<T>) => R): Swap1<T, R>;
-declare function run<T, A, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<A>) => R]): Swap2<T, A, R>;
-declare function run<T, A, B, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<A>) => B, (this: C, value: Resolve<B>) => R]): Swap3<T, A, B, R>;
+declare function run<T, R, C>(this: C, value: T, callback: (this: C, value: Resolve<T>) => R):
+	IfPromise<T, Promisify<R>, R>;
+declare function run<T, A, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<A>) => R]):
+	IfPromise<T & A, Promisify<R>, R>;
+declare function run<T, A, B, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<A>) => B, (this: C, value: Resolve<B>) => R]):
+	IfPromise<T & A & B, Promisify<R>, R>;
 /**
  * Calls the passed callback ‒ forwarding the value and routing back whatever is returned ‒ if the passed value is not
  * null-ish. If the passed value is null-ish, it is returned directly and the passed callback is skipped.
  */
-declare function runIf<T, R, C>(this: C, value: T, callback: (this: C, value: Exclude<Resolve<T>, Nullish>) => R): Swap1<Exclude<T, Nullish>, R> | Extract<T, Nullish> | ExtractResolved<T, Nullish>;
-declare function runIf<T, A, R, C>(this: C, value: T, ...callback: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<A>, Nullish>) => R]): Swap2<Exclude<T, Nullish>, A, R> | Extract<T, Nullish> | ExtractResolved<T, Nullish>;
-declare function runIf<T, A, B, R, C>(this: C, value: T, ...callback: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<A>, Nullish>) => B, (this: C, value: Exclude<Resolve<B>, Nullish>) => R]): Swap3<Exclude<T, Nullish>, A, B, R> | Extract<T, Nullish> | ExtractResolved<T, Nullish>;
+declare function runIf<T, R, C>(this: C, value: T, callback: (this: C, value: Exclude<Resolve<T>, Nullish>) => R): Ever<
+	Extract<T, Nullish> | ExtractResolved<T, Nullish>
+	| IfPromise<Exclude<T, Nullish>, Promisify<R>, R>
+>;
+declare function runIf<T, A, R, C>(this: C, value: T, ...callback: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<A>, Nullish>) => R]): Ever<
+	Extract<T, Nullish> | ExtractResolved<T, Nullish>
+	| IfPromise<Exclude<T, Nullish>, Promise<Extract<A, Nullish>>, Extract<A, Nullish>> | ExtractResolved<A, Nullish>
+	| IfPromise<Exclude<T & A, Nullish>, Promisify<R>, R>
+>;
+declare function runIf<T, A, B, R, C>(this: C, value: T, ...callback: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<A>, Nullish>) => B, (this: C, value: Exclude<Resolve<B>, Nullish>) => R]): Ever<
+	Extract<T, Nullish> | ExtractResolved<T, Nullish>
+	| IfPromise<Exclude<T, Nullish>, Promise<Extract<A, Nullish>>, Extract<A, Nullish>> | ExtractResolved<A, Nullish>
+	| IfPromise<Exclude<T & A, Nullish>, Promise<Extract<B, Nullish>>, Extract<B, Nullish>> | ExtractResolved<B, Nullish>
+	| IfPromise<Exclude<T & A & B, Nullish>, Promisify<R>, R>
+>;
 /**
  * Calls the passed callback, forwarding the value and returning it afterwards.
  */
-declare function apply<T, R, C>(this: C, value: T, callback: (this: C, value: Resolve<T>) => R): Swap1<R, T>;
-declare function apply<T, A, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<T>) => R]): Swap2<A, R, T>;
-declare function apply<T, A, B, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<T>) => B, (this: C, value: Resolve<T>) => R]): Swap3<A, B, R, T>;
+declare function apply<T, R, C>(this: C, value: T, callback: (this: C, value: Resolve<T>) => R):
+	IfPromise<R, Promisify<T>, T>;
+declare function apply<T, A, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<T>) => R]):
+	IfPromise<A & R, Promisify<T>, T>;
+declare function apply<T, A, B, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Resolve<T>) => A, (this: C, value: Resolve<T>) => B, (this: C, value: Resolve<T>) => R]):
+	IfPromise<A & B & R, Promisify<T>, T>;
 /**
  * Calls the passed callback ‒ forwarding the value and returning it afterwards ‒ if the passed value is not null-ish.
  * If the passed value is null-ish, it is returned directly and the passed callback is skipped.
  */
-declare function applyIf<T, R, C>(this: C, value: T, callback: (this: C, value: Exclude<Resolve<T>, Nullish>) => R): Swap1<R, Exclude<T, Nullish>> | Extract<T, Nullish>;
-declare function applyIf<T, A, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<T>, Nullish>) => R]): Swap2<A, R, Exclude<T, Nullish>> | Extract<T, Nullish>;
-declare function applyIf<T, A, B, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<T>, Nullish>) => B, (this: C, value: Exclude<Resolve<T>, Nullish>) => R]): Swap3<A, B, R, Exclude<T, Nullish>> | Extract<T, Nullish>;
+declare function applyIf<T, R, C>(this: C, value: T, callback: (this: C, value: Exclude<Resolve<T>, Nullish>) => R):
+	Extract<T, Nullish> | IfPromise<R, Promisify<Exclude<T, Nullish>>, Exclude<T, Nullish>>;
+declare function applyIf<T, A, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<T>, Nullish>) => R]):
+	Extract<T, Nullish> | IfPromise<A & R, Promisify<Exclude<T, Nullish>>, Exclude<T, Nullish>>;
+declare function applyIf<T, A, B, R, C>(this: C, value: T, ...callbacks: [(this: C, value: Exclude<Resolve<T>, Nullish>) => A, (this: C, value: Exclude<Resolve<T>, Nullish>) => B, (this: C, value: Exclude<Resolve<T>, Nullish>) => R]):
+	Extract<T, Nullish> | IfPromise<A & B & R, Promisify<Exclude<T, Nullish>>, Exclude<T, Nullish>>;
 
 export {
 	run, runIf,
